726812aa65b77b2df7a33069bdf777be4f404cd1
Igres111 <sergikaralashvili@gmail.com>
2024-07-08 19:32:55 +0400

    finished structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
+import bg_mobile from "/images/bg-main-mobile.png";
 
 function App() {
   return (
-    <div>
-      <div></div>
+    <div className="h-screen w-screen">
+      <div>
+        <img src={bg_mobile} />
+      </div>
+      <div>
+        <h4>Cardholder Name</h4>
+        <input type="text" placeholder="e.g. Jane Appleseed" />
+      </div>
+      <div>
+        <h4>Card Number</h4>
+        <input type="text" placeholder="e.g. 1234 5678 9123 0000"></input>
+      </div>
+      <div>
+        <h4>Exp. Date (MM/YY)</h4>
+        <div>
+          <input type="text" placeholder="MM" />
+          <input type="text" placeholder="YY" />
+        </div>
+        <div>
+          <h4>CVC</h4>
+          <input type="text" placeholder="e.g. 123" />
+        </div>
+      </div>
+      <button type="submit" />
     </div>
   );
 }
